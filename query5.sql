3146f460e083d41139099641504b996848e67cf3
Emkot87 <49078291+Emkot87@users.noreply.github.com>
2022-12-13 09:51:29 +0200

    Update query5.sql

--- query5.sql
+++ query5.sql
@@ -1,4 +1,4 @@
-SELECT product.id, supplier.company_name, supplier.email
+SELECT products.id, supplier.company_name, supplier.email
 FROM products JOIN stock ON products.id = stock.product_id 
 WHERE stock.expire_date < "19/11/2022" 
 JOIN suppliers ON products.supplier = suppliers.company_name;
