a7b60cfd34a243dbdb931f22f2b8e123b7d9dd0c
Emkot87 <49078291+Emkot87@users.noreply.github.com>
2022-12-13 09:50:58 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,3 +1,3 @@
-SELECT product.name, stock.expire_date
-FROM product JOIN stock ON product.id = stock.product_id
-WHERE product.supplier = "Mebgal AE" AND stock.order_date = "13/05/2020" 
+SELECT products.name, stock.expire_date
+FROM products JOIN stock ON products.id = stock.product_id
+WHERE products.supplier = "Mebgal AE" AND stock.order_date = "13/05/2020" 
